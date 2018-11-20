ab2316520780ebf1e81e1e68d9ccd3bc84902f19
Martin Tirtawsata <martin.tirtawisata@hotmail.com>
2018-11-20 12:50:33 -0800

    cors

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://rocky-castle-33666.herokuapp.com/api/ww';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://rocky-castle-33666.herokuapp.com/api';
+// this route is used in Heroku
