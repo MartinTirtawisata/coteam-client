fee7d5bfee59e8107f94edf3dcae9a678e2d7e88
Martin Tirtawsata <martin.tirtawisata@hotmail.com>
2018-11-19 22:49:00 -0800

    fix test

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
+import {Provider} from 'react-redux';
+import store from '../store';
 
 import App from './app';
 
 describe('<App />', () => {
     it('Renders without crashing', () => {
-        shallow(<App />);
+        shallow(<Provider store={store}>
+            <App />
+        </Provider>)
     });
 });
